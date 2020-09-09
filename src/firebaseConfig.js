b64124c7ace2140fd7721611aaaaff60391fd144
Rafsun4 <saifrafsun@gmail.com>
2020-09-09 13:11:54 +0600

    first commit with firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAJHg3tPvSnJjzxjEI4lNYkIBIriInCCRA",
+  authDomain: "ema-john-with-auth.firebaseapp.com",
+  databaseURL: "https://ema-john-with-auth.firebaseio.com",
+  projectId: "ema-john-with-auth",
+  storageBucket: "ema-john-with-auth.appspot.com",
+  messagingSenderId: "462679315189",
+  appId: "1:462679315189:web:4faa098aa63f432d212bbc",
+  measurementId: "G-VLRQ38KP0D"
+};
+export default firebaseConfig;
